f0f26b2bcf264ea6bccee27ec8c574be9d38cadf
Shiplayer <lllytep199515@yandex.ru>
2016-05-21 18:59:18 +0300

    gradle and .idea

--- app/src/main/java/com/advanced/java/ship/dictionary/Activity/WordListActivity.java
+++ app/src/main/java/com/advanced/java/ship/dictionary/Activity/WordListActivity.java
@@ -1,9 +1,17 @@
 package com.advanced.java.ship.dictionary.Activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.advanced.java.ship.dictionary.R;
+import com.advanced.java.ship.dictionary.TranslatedWord;
+
+import java.util.List;
 
 public class WordListActivity extends AppCompatActivity {
     private ListView list;
@@ -22,13 +30,13 @@ public class WordListActivity extends AppCompatActivity {
         private List<TranslatedWord> list;
 
         public ArrayWordAdapter(Context context, List<TranslatedWord> list){
-            super(context, R.id.list_item_view, list);
+            super(context, R.layout.list_item_view, list);
             this.context = context;
             this.list = list;
         }
 
         public View getView(int position, View convertView, ViewGroup parent) {
-            
+            return null;
         }
     }
 }
